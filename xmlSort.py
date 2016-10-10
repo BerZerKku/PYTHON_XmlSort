959c13c214da157462d1d3e601cde77cbe4e4bd0
bear <bear@prosoftsystems.ru>
2016-10-10 15:01:45 +0500

    Считывание трех словарей сделано через одну функцию.
    Signed-off-by: bear <bear@prosoftsystems.ru>

--- xmlSort.py
+++ xmlSort.py
@@ -2,60 +2,78 @@
 from lxml import etree
 import zipfile
 
-DATE_END = "31.12.2999"
-
+HANDBOOK = {
+# SPHVID
 # IDHVID -> HVIDNAME   
 # Example:
 # "14.00.26.003" -> "Коронарная реваскуляризация миокарда с применением
 # ангиопластики в сочетании со стентированием при ишемической болезни
 # сердца"
-SPHVID = {}
+    "SPHVID":{
+        'keys':  ("IDHVID",),
+        'value': "HVIDNAME",
+        'date':  "DATEEND"
+    },
 
+# SPVMPSERV 
 # CODE -> N_GR
 # Example:
 # "09.00.15.002.331" -> "15"
-SPVMPSERV = {}
+    "SPVMPSERV":{
+        'keys':  ("CODE",),
+        'value': "N_GR",
+        'date':  "DEND"
+    },
+# SPHMET
+# HVID.IDHM -> HMNAME
+# Example:
+# "11.00.21.006.399" -> "исправление косоглазия с пластикой экстраокулярных мышц"
+# 
+    "SPHMET":{
+        'keys':  ("HVID", "IDHM"), 
+        'value': "HMNAME",
+        'date':  "DATEEND"
+    }
+}
 
-def readSPHVID():
-    global SPHVID
+# дата окончания действия
+DATE_END = "31.12.2999"
+
+
+def readHandbook(name, fields):
+    ''' (str, dict) -> None
+
+        Считывается из заархивированного файла \a name ".zip" одноименного файла
+        ".xml". Затем формируется словарь с тем же именем, в котором хранятся
+        данные извлеченные согласно правилам заданным в \a fields:
+        'keys' - ключевое поля (или поля, которые собираются через точку),
+        'value' - значение ключев,
+        'date' - проверка на актуальность даты (\a DATE_END).   
+    '''
     
-    archive = zipfile.ZipFile('SPHVID.zip', 'r')
-    f = archive.open('SPHVID.XML')
+    archive = zipfile.ZipFile(name + '.zip', 'r')
+    f = archive.open(name + '.XML')
 
     parser = etree.XMLParser(encoding='cp1251')
     page = etree.parse(f, parser)
     nodes = page.xpath('/ROOT/REC')
 
     for node in nodes:
-        dateend = node.get('DATEEND')
+        dateend = node.get(fields['date'])
         if dateend == DATE_END:
-           idhvid = node.get('IDHVID') 
-           hvidname = node.get('HVIDNAME')
-           SPHVID.update({idhvid: hvidname})
+            key = ''
+
+            for k in fields['keys']:
+                key += node.get(k)
+                key += '.'
+            key = key[0:-1]
+            value = node.get(fields['value'])
+            globals()[name][key] = value
 
 
-def readSPVMPSERV():
-    global SPVMPSERV
+for name in HANDBOOK:
+    globals()[name] = {}
+    readHandbook(name, HANDBOOK[name])          
 
-    archive = zipfile.ZipFile('SPVMPSERV.zip', 'r')
-    f = archive.open('SPVMPSERV.XML')
-
-    parser = etree.XMLParser(encoding='cp1251')
-    page = etree.parse(f, parser)
-    nodes = page.xpath('/ROOT/REC')
-
-    for node in nodes:
-        dend = node.get('DEND')
-        if dend == DATE_END:
-           code = node.get('CODE') 
-           n_gr = node.get('N_GR')
-           SPVMPSERV.update({code: n_gr})
-    
-
-readSPHVID()
-#for key in SPHVID:
-#    print key, SPHVID.get(key)
-
-readSPVMPSERV()
-#for key in SPVMPSERV:
-#    print key, SPVMPSERV.get(key)    
+#for key in SPHMET:
+#    print key, SPHMET.get(key)   
