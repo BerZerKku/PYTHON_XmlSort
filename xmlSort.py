d3e97104bf85fee46678ddf95feb9f30e14830e3
bear <bear@prosoftsystems.ru>
2016-10-10 18:01:12 +0500

    Добавлено формирование конечной таблицы. Начата работа над сохранением в файл.
    Signed-off-by: bear <bear@prosoftsystems.ru>

--- xmlSort.py
+++ xmlSort.py
@@ -2,15 +2,21 @@
 from lxml import etree
 import zipfile
 
+# дата окончани€ действи€
+DATE_END = "31.12.2999"
+
+# необходимые отделени€
+DIVISION = ("106", "156")
+
 HANDBOOK = {
 # SPHVID
 # IDHVID -> HVIDNAME   
 # Example:
-# "14.00.26.003" -> " оронарна€ реваскул€ризаци€ миокарда с применением
+# "14.00.26.003" -> {IDHVID=" оронарна€ реваскул€ризаци€ миокарда с применением
 # ангиопластики в сочетании со стентированием при ишемической болезни
-# сердца"
+# сердца"}
     "SPHVID":{
-        'keys':     ("IDHVID",),
+        'keys':     "IDHVID",
         'values':   ("HVIDNAME",),
         'date':     "DATEEND"
     },
@@ -20,28 +26,41 @@ HANDBOOK = {
 # Example:
 # "09.00.15.002.331" -> "15"
     "SPVMPSERV":{
-        'keys':     ("CODE",),
+        'keys':     "CODE",
         'values':   ("N_GR",),
         'date':     "DEND"
     },
 # SPHMET
-# HVID.IDHM -> HMNAME
+# HVID + '.' + IDHM -> HMNAME
 # Example:
-# "11.00.21.006.399" -> "исправление косоглази€ с пластикой экстраокул€рных мышц"
-# 
+# "11.00.21.006.399" -> {"N_GR" = "исправление косоглази€ с пластикой
+# экстраокул€рных мышц"}
     "SPHMET":{
         'keys':     ("HVID", "IDHM"), 
         'values':   ("HMNAME",),
         'date':     "DATEEND"
-    }
+    },
 # SPMEDSERVICE
 # DIVISION -> {CODE, NAME}
 # Example:
-# 
+# "106" -> {CODE="10.00.19.004.371",NAME="операции по реиннервации и заместительной
+# функциональной пластике гортани и трахеи с применением микрохирургической техники
+# и электромиографическим мониторингом;–еконструктивно-пластическое восстановление
+# функции гортани и трахеи"}
+    "SPMEDSERVICE":{
+        'keys':     "CODE",
+        'values':   ("DIVISION", "NAME"),
+        'date':     "DEND"
+    }
 }
 
-# дата окончани€ действи€
-DATE_END = "31.12.2999"
+# ¬нешние справочники
+SPHVID = {}
+SPHMET = {}
+SPHMET = {}
+SPMEDSERVICE = {}
+# —формированные справочники
+GLB_HANDBOOK = {}
 
 ##
 def getNodesFromXml(name):
@@ -65,6 +84,7 @@ def getNodesFromXml(name):
     f.close()
     return nodes
 
+
 ##
 def readHandbook(name, nodes, fields):
     ''' (str, list, dict) -> None
@@ -76,19 +96,34 @@ def readHandbook(name, nodes, fields):
         'value' - значени€ ключей,
         'date' - проверка на актуальность даты (\a DATE_END).   
     '''
+    print "Cправочник %s." % (name),
     for node in nodes:
         dateend = node.get(fields['date'])
-        if dateend == DATE_END:
-            key = ''
-
+        if dateend != DATE_END:
+            continue
+        
+        division = node.get('DIVISION')
+        if division is not None:
+            if not (division in DIVISION):
+                continue
+            
+        key = ''
+        if type(fields['keys']) == str:
+            key = node.get(fields['keys'])
+        else :
             for k in fields['keys']:
                 key += node.get(k)
                 key += '.'
             key = key[0:-1]
-            values = {}
-            for value in fields['values']:
-                values[value] = node.get(value)
-            globals()[name][key] = values
+        values = {}
+        for value in fields['values']:
+            values[value] = node.get(value)
+        globals()[name].update({key: values})
+
+    l1 = len(globals()[name])
+    l2 = len(nodes)
+    print "—читано %s из %s." % (l1, l2)
+
 
 ##
 def printHandbook(name):
@@ -99,7 +134,61 @@ def printHandbook(name):
     for key in eval(name):
         print key,
         for value in eval(name)[key]:
-            print eval(name)[key].get(value)
+            print "value = ", eval(name)[key].get(value),
+            print
+
+
+##
+def createHandbook():
+    global GLB_HANDBOOK
+    ''' (None) -> None
+
+        ‘ормирование конечного словар€:
+        CODE -> dict{DIVISION, NAME, HVIDNAME, HMNAME, N_GR}
+    '''
+
+    for code in SPMEDSERVICE:
+        data = {}
+        name = SPMEDSERVICE[code].get("NAME")
+        data["NAME"] = name;
+
+        hmname = 'None'
+        if code in SPHMET:
+            hmname = SPHMET[code].get("HMNAME")
+        data["HMNAME"] = hmname;
+
+        #pos = code.rfind('.')
+        idhvid = code[0:code.rfind('.')] 
+        hvidname = 'None'
+        if code[0:code.rfind('.')] in SPHVID:
+            hvidname = SPHVID[idhvid].get("HVIDNAME")
+        data["HVIDNAME"] = hvidname
+        
+        n_gr = 'None'
+        if code in SPVMPSERV:
+            n_gr = SPVMPSERV[code].get("N_GR")
+        data["N_GR"] = n_gr
+        GLB_HANDBOOK.update({code: data})
+            
+
+def saveFile(name):
+    f = open(name, "w")
+
+    cnt = 0
+    for code in GLB_HANDBOOK:
+        cnt += 1
+        if cnt >= 100:
+            break;
+        name = GLB_HANDBOOK[code].get("NAME")
+        hmname = GLB_HANDBOOK[code].get("HMNAME")
+        hvidname = GLB_HANDBOOK[code].get("HVIDNAME")
+        n_gr = GLB_HANDBOOK[code].get("N_GR")
+        s = code + ',' + name + ',' + hmname + ',' + hvidname + ',' + n_gr + '\n'
+        f.write(s.encode("cp1251"))
+        
+    f.close()
+    
+
 
 ##
 if __name__ == "__main__":
@@ -107,8 +196,12 @@ if __name__ == "__main__":
         globals()[name] = {}
         nodes = getNodesFromXml(name)
         readHandbook(name, nodes, HANDBOOK[name])
+    
+    #printHandbook("SPVMPSERV")
 
-    printHandbook("SPHMET")
+    createHandbook()
+
+    saveFile(u"file.txt")
      
 
 
